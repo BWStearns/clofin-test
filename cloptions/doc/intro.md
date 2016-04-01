1309879435b913d8fe08180bd53a88ae2a906f5f
Brian Stearns <brianw.stearns@gmail.com>
2016-04-01 14:29:47 -0400

    initial

---
+++ cloptions/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cloptions
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ cloptions/test/cloptions/core_test.clj
@@ -0,0 +1,7 @@
+(ns cloptions.core-test
+  (:require [clojure.test :refer :all]
+            [cloptions.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
